8f823e63b8596c14ae3d0846984d29436b726b25
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-01-11 02:31:40 +0300

    Complete ch06 ex08

---
+++ ch06-flow-control/ex08-numberRange.js
@@ -0,0 +1,18 @@
+// Write a function that logs whether a number is between 0 and 50 (inclusive), between 51 and 100 (inclusive), greater than 100, or less than 0.
+
+function numberRange(number) {
+  if (number >= 0 && number <= 50) {
+    console.log(`${number} is between 0 and 50 `);
+  } else if (number >= 51 && number <= 100) {
+    console.log(`${number} is between 51 and 100 `);
+  } else if (number > 100) {
+    console.log(`${number} is greater than 100`);
+  } else {
+    console.log(`${number} is less than 0`);
+  }
+}
+
+numberRange(25);
+numberRange(75);
+numberRange(125);
+numberRange(-25);
