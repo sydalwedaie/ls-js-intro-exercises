0e2ac682dff5cf3fa1ef8ccf979f49777220ffd4
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-01-12 19:33:29 +0300

    Complete ch07 ex06

---
+++ ch07-loops/ex06-factorial.js
@@ -0,0 +1,17 @@
+//Reimplement the factorial function from exercise 2 using recursion. Once again, you may assume that the argument is always a positive integer.
+
+
+function factorial(num) {
+  if (num === 1) return 1;
+  return num * factorial(num-1);
+}
+
+console.log(factorial(1));     // => 1
+console.log(factorial(2));     // => 2
+console.log(factorial(3));     // => 6
+console.log(factorial(4));     // => 24
+console.log(factorial(5));     // => 120
+console.log(factorial(6));     // => 720
+console.log(factorial(7));     // => 5040
+console.log(factorial(8));     // => 40320
+console.log(factorial(80));  
