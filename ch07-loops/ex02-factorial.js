1ff297ab12aacaaf4f73dd2561d9ef58768ab247
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-01-12 05:03:44 +0300

    Complete ch07 ex02

---
+++ ch07-loops/ex02-factorial.js
@@ -0,0 +1,18 @@
+// Write a function that computes and returns the factorial of a number by using a for loop. The factorial of a positive integer n, signified by n!, is defined as the product of all integers between 1 and n, inclusive:
+
+function factorial(num) {
+  let result = 1;
+  for (let i = num; i > 0; i -= 1) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log(factorial(1));     // => 1
+console.log(factorial(2));     // => 2
+console.log(factorial(3));     // => 6
+console.log(factorial(4));     // => 24
+console.log(factorial(5));     // => 120
+console.log(factorial(6));     // => 720
+console.log(factorial(7));     // => 5040
+console.log(factorial(8));     // => 40320
