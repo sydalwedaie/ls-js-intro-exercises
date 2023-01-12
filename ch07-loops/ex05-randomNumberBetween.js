a21997c73d7362277b8730cbbcba730acb52e8b7
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-01-12 19:33:11 +0300

    Complete ch07 ex05

---
+++ ch07-loops/ex05-randomNumberBetween.js
@@ -0,0 +1,14 @@
+function randomNumberBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let tries = 0;
+let result = null;
+
+while (result <= 2) {
+  result = randomNumberBetween(1, 6);
+  tries += 1;
+}
+
+console.log(result);
+console.log('It took ' + String(tries) + ' tries to get a number greater than 2');
