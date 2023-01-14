d788b3b6c82dc26aa1ac7c2ac3f738e43a9d5159
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-01-14 03:48:45 +0300

    Complete ch08 ex05

---
+++ ch08-arrays/ex05.js
@@ -0,0 +1,9 @@
+// Write a findIntegers function that takes an array argument and returns an array that contains only the integers from the input array. Use the filter method in your function.
+
+let things = [1, 'a', '1', 3, NaN, 3.1415, -4, null, false];
+
+function findIntegers(array) {
+  return array.filter(item => Number.isInteger(item));
+}
+
+console.log(findIntegers(things));
