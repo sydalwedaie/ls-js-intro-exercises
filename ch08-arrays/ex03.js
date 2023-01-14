f952d0f9541b703a3070b3c7f54731c994d21be6
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-01-14 03:39:17 +0300

    Complete ch08 ex03

---
+++ ch08-arrays/ex03.js
@@ -0,0 +1,9 @@
+// Let's make the problem a little harder. In this problem, we're again interested in even numbers, but this time the numbers are in nested arrays in a single outer array. Log them out.
+
+let myArray = [
+  [1, 3, 6, 11],
+  [4, 2, 4],
+  [9, 17, 16, 0],
+];
+
+myArray.forEach(a => a.filter(a => a%2 === 0).forEach(a => console.log(a)));
