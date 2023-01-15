8632c21c8f18a61cf2790b932d21d21456847f39
Sayed Alwedaie <sydalwedaie@gmail.com>
2023-01-15 23:39:46 +0300

    Complete ch09 ex12

---
+++ ch09-objects/ex12.js
@@ -0,0 +1,12 @@
+// The following code will raise an error on line 6. Without creating a new function or changing lines 5 or 6, update this code to work as intended.
+
+function foo(bar) {
+  console.log(bar, bar, bar);
+}
+
+let bar = Object.assign(foo);
+// also works: bar = foo;
+foo("hello"); // should print "hello hello hello"
+bar("hi");    // should print "hi hi hi"
+
+
